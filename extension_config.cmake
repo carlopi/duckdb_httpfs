200c879579b107bd9deb0a5a04d192b3a21e90c3
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-05-31 12:29:17 +0200

    spatial / 2

--- extension_config.cmake
+++ extension_config.cmake
@@ -22,8 +22,8 @@ duckdb_extension_load(httpfs
 	
 duckdb_extension_load(spatial
     DONT_LINK LOAD_TESTS
-    GIT_URL https://github.com/duckdb/duckdb_spatial.git
-    GIT_TAG 6826755ec32aea77777b136cc81683fa9be4df7b
+    GIT_URL https://github.com/carlopi/duckdb_spatial.git
+    GIT_TAG main
     INCLUDE_DIR spatial/include
     TEST_DIR test/sql
     )
